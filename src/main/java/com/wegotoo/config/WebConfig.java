bf01050f8def2e8683e87c1f540def755ce22158
jwooo <parkjwoo0515@gmail.com>
2024-09-11 17:19:35 +0900

    feat: Cors 설정 (#12)

---
+++ src/main/java/com/wegotoo/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.wegotoo.config;
+
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name())
+                .allowCredentials(true)
+                .exposedHeaders("*");
+    }
+
+}
