b708b8a1a7fe3e8921ffa0f3f296d5e40309767b
jwooo <parkjwoo0515@gmail.com>
2024-09-12 16:41:13 +0900

    feat: HTTP Method OPTIONS 추가 (#12)

--- src/main/java/com/wegotoo/config/WebConfig.java
+++ src/main/java/com/wegotoo/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.wegotoo.config;
 
 import static org.springframework.http.HttpMethod.DELETE;
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
 import static org.springframework.http.HttpMethod.PATCH;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.HttpMethod.PUT;
@@ -17,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name())
+                .allowedMethods(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name(), OPTIONS.name())
                 .allowCredentials(true)
                 .exposedHeaders("*");
     }
