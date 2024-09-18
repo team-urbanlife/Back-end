7f6b75d6f4c21c0cf32b47ea32a114c7b582efae
jwooo <parkjwoo0515@gmail.com>
2024-09-18 17:41:21 +0900

    refactor: 채팅방 생성 API 기능 수정 (#26)

--- src/main/java/com/wegotoo/domain/chatroom/repository/ChatRoomRepository.java
+++ src/main/java/com/wegotoo/domain/chatroom/repository/ChatRoomRepository.java
@@ -1,7 +1,16 @@
 package com.wegotoo.domain.chatroom.repository;
 
 import com.wegotoo.domain.chatroom.ChatRoom;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("SELECT c FROM ChatRoom c " +
+            "JOIN UserChatRoom ucr ON ucr.chatRoom = c " +
+            "WHERE ucr.user.id = :userId AND ucr.accompany.id = :accompanyId")
+    Optional<ChatRoom> findByUserIdAndAccompanyId(@Param("userId") Long userId, @Param("accompanyId") Long accompanyId);
+
 }
