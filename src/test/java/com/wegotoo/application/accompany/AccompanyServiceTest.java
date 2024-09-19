5547bfbcfb163badb1e19ca7eeeb9c01484db243
ahnyunki <ahnyunki00@gmail.com>
2024-09-20 07:49:53 +0900

    test: 여행 동행 구인 글 수정 API 테스트 코드 작성 (#32)

--- src/test/java/com/wegotoo/application/accompany/AccompanyServiceTest.java
+++ src/test/java/com/wegotoo/application/accompany/AccompanyServiceTest.java
@@ -4,6 +4,7 @@ import static com.wegotoo.domain.accompany.Gender.NO_MATTER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.wegotoo.api.accompany.request.AccompanyEditServiceRequest;
 import com.wegotoo.application.ServiceTestSupport;
 import com.wegotoo.application.accompany.request.AccompanyCreateServiceRequest;
 import com.wegotoo.domain.accompany.Accompany;
@@ -67,8 +68,62 @@ class AccompanyServiceTest extends ServiceTestSupport {
         // then
         List<Accompany> response = accompanyRepository.findAll();
         assertThat(response.get(0))
-                .extracting("id", "title", "location", "latitude", "longitude", "personnel", "gender", "startAge", "endAge", "cost")
+                .extracting("id", "title", "location", "latitude", "longitude", "personnel", "gender", "startAge",
+                        "endAge", "cost")
                 .contains(response.get(0).getId(), "제주도 여행 모집", "제주도", 0.0, 0.0, 3, NO_MATTER, 20, 29, 1000000);
+    }
+
+    @Test
+    @DisplayName("유저가 여행 모집글을 수정한다.")
+    void editAccompany() throws Exception {
+        // given
+        User user = User.builder()
+                .email("user@gmail.com")
+                .name("user")
+                .build();
+        userRepository.save(user);
+
+        Accompany accompany = Accompany.builder()
+                .startDate(START_DATE)
+                .endDate(END_DATE)
+                .title("제주도 여행 모집")
+                .location("제주도")
+                .latitude(0.0)
+                .longitude(0.0)
+                .personnel(3)
+                .gender(NO_MATTER)
+                .startAge(20)
+                .endAge(29)
+                .cost(1000000)
+                .content("여행 관련 글")
+                .user(user)
+                .build();
+        accompanyRepository.save(accompany);
+
+        AccompanyEditServiceRequest request = AccompanyEditServiceRequest.builder()
+                .startDate(START_DATE)
+                .endDate(END_DATE)
+                .title("제주도 여행 모집 수정")
+                .location("제주도 수정")
+                .latitude(0.0)
+                .longitude(0.0)
+                .personnel(3)
+                .gender(NO_MATTER)
+                .startAge(20)
+                .endAge(29)
+                .cost(1000000)
+                .content("여행 관련 글 수정")
+                .build();
+        // when
+        accompanyService.editAccompany(user.getId(), accompany.getId(), request);
+
+        // then
+        List<Accompany> response = accompanyRepository.findAll();
+        assertThat(response.get(0))
+                .extracting("id", "title", "location", "latitude", "longitude", "personnel", "gender", "startAge",
+                        "endAge", "cost", "content")
+                .contains(response.get(0).getId(), "제주도 여행 모집 수정", "제주도 수정", 0.0, 0.0, 3, NO_MATTER, 20, 29, 1000000,
+                        "여행 관련 글 수정");
     }
 
 }
