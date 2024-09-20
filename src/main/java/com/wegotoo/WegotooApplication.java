fe23d97e95ed1edcd07fc54245a34144b6b8748a
ahnyunki <ahnyunki00@gmail.com>
2024-09-20 19:10:32 +0900

    feat: 로그 추적기 생성 (#39)

--- src/main/java/com/wegotoo/WegotooApplication.java
+++ src/main/java/com/wegotoo/WegotooApplication.java
@@ -1,13 +1,21 @@
 package com.wegotoo;
 
+import com.wegotoo.trace.logtrace.LogTrace;
+import com.wegotoo.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WegotooApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WegotooApplication.class, args);
+    }
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 
 }
