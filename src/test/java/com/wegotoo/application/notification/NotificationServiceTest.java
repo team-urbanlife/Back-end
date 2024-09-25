96f61c2777e1619555e7dcf3cd8302371d30188f
ahnyunki <ahnyunki00@gmail.com>
2024-09-25 22:06:56 +0900

    test: SSE를 활용하여 채팅 알림 테스트 코드 작성 (#54)

---
+++ src/test/java/com/wegotoo/application/notification/NotificationServiceTest.java
@@ -0,0 +1,27 @@
+package com.wegotoo.application.notification;
+
+import java.io.IOException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceTest {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @Test
+    @DisplayName("A유저가 접속한 상태에서 메시지 알림을 전송 받을 수 있다.")
+    public void testNotifyChatting() throws IOException {
+        // given
+        Long receiverId = 1L;
+
+        notificationService.subscribe(receiverId);
+
+        // when
+        notificationService.notifyChatting(receiverId, "");
+
+    }
+}
