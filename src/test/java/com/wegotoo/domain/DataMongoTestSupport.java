f2ff79a57bac767d3722722a57a9459b32fb6568
jwooo <parkjwoo0515@gmail.com>
2024-11-05 17:03:28 +0900

    test: DataMongoTestSupport PostInspector 추가 (#96)

--- src/test/java/com/wegotoo/domain/DataMongoTestSupport.java
+++ src/test/java/com/wegotoo/domain/DataMongoTestSupport.java
@@ -1,11 +1,12 @@
 package com.wegotoo.domain;
 
-import com.wegotoo.config.EmbeddedMongoConfig;
+import com.wegotoo.config.embedded.EmbeddedMongoConfig;
 import com.wegotoo.config.MongoConfig;
+import com.wegotoo.config.embedded.PortInspector;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Import;
 
 @DataMongoTest
-@Import({MongoConfig.class, EmbeddedMongoConfig.class})
+@Import({MongoConfig.class, EmbeddedMongoConfig.class, PortInspector.class})
 public abstract class DataMongoTestSupport {
 }
